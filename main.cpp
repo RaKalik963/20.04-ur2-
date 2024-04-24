222555691a6a613bc7c1f66ddef451ce2761bef5
RaKalik963 <andrejmahot646@gmail.com>
2024-04-24 20:25:41 +0300

    lab-ex2

--- main.cpp
+++ main.cpp
@@ -4,8 +4,18 @@ using namespace std;
 HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 /*SetConsoleTextAttribute(handle, 0x0);
 */
-
+/*"U lukomor'a dub zelenij,
+ Zlataya zep na dupe tom,
+ I dnem i nochju kot uchenij
+ vse hodit po cepi krugom"
+ */
 int main()
 {
-
+	SetConsoleTextAttribute(handle, 0x06);
+	cout << "\"U lukomor'a dub zelenij,\a" << endl;
+    cout << "Zlataya zep na dupe tom,\a" << endl;
+    cout << "I dnem i nochju kot uchenij\a" << endl;
+    cout << "vse hodit po cepi krugom\a\"" << endl;
+	system("pause");
+	return 0;
 }
