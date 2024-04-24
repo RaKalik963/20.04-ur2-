826b73b7980a2c6424efaee094b65db12e800ca7
RaKalik963 <andrejmahot646@gmail.com>
2024-04-24 19:58:28 +0300

    dz-ex2

--- main.cpp
+++ main.cpp
@@ -4,8 +4,14 @@ using namespace std;
 HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 /*SetConsoleTextAttribute(handle, 0x0);
 */
-
+/*S = pi*R2  L=2*pi *R.
+*/
 int main()
 {
-
+	double pi = 3.14;
+	double R = 5;
+	cout << "S = " << pi * (R * R) << endl;
+	cout << "L = " << 2 * pi * R << endl;
+	system("pause");
+	return 0;
 }
